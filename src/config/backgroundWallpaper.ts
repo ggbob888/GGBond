ce8cd6bccb906310d9a66984109222a3ea336e9a
ggbob888 <1187936812@qq.com>
2026-09-06 20:11:13 +0800

    更换网站主页壁纸

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -39,12 +39,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/GGBondDesktop/GGBond1.avif",
+			"assets/images/GGBondDesktop/GGBond2.avif",
+			"assets/images/GGBondDesktop/GGBond3.avif",
+			"assets/images/GGBondDesktop/GGBond4.avif",
+			"assets/images/GGBondDesktop/GGBond5.avif",
+			"assets/images/GGBondDesktop/GGBond6.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
